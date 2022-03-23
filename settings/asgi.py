2d567143626548edcbc90cd6f2bb2e630f2bd9bf
gofven <cratedcreations@gmail.com>
2022-03-23 16:43:19 +0100

    Added djang_asgi_app variable to asgi.py

--- settings/asgi.py
+++ settings/asgi.py
@@ -2,11 +2,14 @@ import os
 
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+
 from .middleware import TokenAuthMiddleware
 
 from flowback.chat import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.base')
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
         'websocket': AllowedHostsOriginValidator(
