0b6a7faaaf0955b2066f81385f882f6922decc90
gofven <cratedcreations@gmail.com>
2022-03-23 16:48:38 +0100

    Rolling update

--- settings/asgi.py
+++ settings/asgi.py
@@ -1,15 +1,14 @@
 import os
-
-from channels.security.websocket import AllowedHostsOriginValidator
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-
-from .middleware import TokenAuthMiddleware
-
-from flowback.chat import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.base')
 django_asgi_app = get_asgi_application()
+
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.routing import ProtocolTypeRouter, URLRouter
+from .middleware import TokenAuthMiddleware
+from flowback.chat import routing
+
 
 application = ProtocolTypeRouter({
         'websocket': AllowedHostsOriginValidator(
