127c76856c8d613c6bbbe56b8f405a3e2d6a3c3f
gofven <cratedcreations@gmail.com>
2022-03-23 17:34:19 +0100

    Rolling update

--- flowback/chat/routing.py
+++ flowback/chat/routing.py
@@ -1,7 +1,7 @@
-from django.urls import re_path
+from django.conf.urls import url
 
 from . import consumers
 
 websockets_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi())
+    url(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi())
 ]
