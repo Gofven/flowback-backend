85fc7bba9e193c8e84242fc993a93012f4e03170
gofven <cratedcreations@gmail.com>
2022-04-12 12:58:43 +0200

    Rolling Update

--- flowback/probability/services.py
+++ flowback/probability/services.py
@@ -9,7 +9,7 @@ def probability_vote_create(*, user: int, post: int, score: int):
     probability_post_check(post=post)
 
     user = ProbabilityUser.objects.get_or_create(user=user)
-    post = get_object_or_404(ProbabilityPost, post=post)
+    post = get_object_or_404(ProbabilityPost, pk=post)
 
     if not (post.active or post.finished):
         return ValidationError('Post is inactive or finished.')
@@ -29,7 +29,7 @@ def probability_vote_delete(*, user: int, post: int):
 
 
 def probability_count_votes(*, post: int):
-    post = get_object_or_404(ProbabilityPost, post=post)
+    post = get_object_or_404(ProbabilityPost, pk=post)
     total_votes = ProbabilityVote.objects.filter(post=post).count()
 
     # Get the average votes, multiply by 20 at the end
@@ -39,7 +39,7 @@ def probability_count_votes(*, post: int):
 
 
 def probability_post_finish(*, post: int):
-    post = get_object_or_404(ProbabilityPost, post=post)
+    post = get_object_or_404(ProbabilityPost, pk=post)
     votes = ProbabilityVote.objects.filter(post=post).all()
 
     # Round values to steps of 20 then divide by 20 to have same values as ProbabilityVote models
@@ -55,7 +55,7 @@ def probability_post_finish(*, post: int):
 
 
 def probability_post_check(*, post: int):
-    post = get_object_or_404(ProbabilityPost, post=post)
+    post = get_object_or_404(ProbabilityPost, pk=post)
 
     probability_count_votes(post=post.id)
 
