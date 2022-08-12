41c7087358710b055ecfb99269193127be60337e
gofven <cratedcreations@gmail.com>
2022-08-12 14:19:46 +0200

    Updated Group Models

--- flowback/group/models.py
+++ flowback/group/models.py
@@ -26,11 +26,10 @@ class Group(BaseModel):
     image = models.ImageField()
     cover_image = models.ImageField()
 
-    tags = models.ManyToManyField('GroupTags')
-
     jitsi_room = models.TextField(unique=True)
 
 
+# Permission class for each Group
 class GroupPermission(BaseModel):
     role_name = models.TextField()
     author = models.ForeignKey('Group', on_delete=models.CASCADE)
@@ -41,27 +40,37 @@ class GroupPermission(BaseModel):
     ban_members = models.BooleanField(default=False)
 
 
+# Permission Tags for each group, and for user to put on delegators
 class GroupTags(BaseModel):
     tag_name = models.TextField()
+    group = models.ForeignKey('Group', on_delete=models.CASCADE)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        unique_together = ('tag_name', 'group')
 
 
+# User information for the specific group
 class GroupUser(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     is_delegate = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
     permission = models.ForeignKey(GroupPermission, null=True, blank=True, on_delete=models.SET_NULL)
 
     class Meta:
         unique_together = ('user', 'group')
 
 
+# User invites
 class GroupUserInvite(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
 
 
+# Delegator to delegate relations
 class GroupUserDelegate(BaseModel):
     delegator = models.ForeignKey(GroupUser, on_delete=models.CASCADE, related_name='group_user_delegate_delegator')
     delegate = models.ForeignKey(GroupUser, on_delete=models.CASCADE, related_name='group_user_delegate_delegate')
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
-    tag = models.ManyToManyField(GroupTags)
+    tags = models.ManyToManyField(GroupTags)
