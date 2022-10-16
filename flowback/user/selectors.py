63ab84cffdddb1f011c6bcb18b81bbda9b89780a
gofven <cratedcreations@gmail.com>
2022-10-16 16:50:44 +0200

    Added option to only allow group admins or higher to list users

--- flowback/user/selectors.py
+++ flowback/user/selectors.py
@@ -1,7 +1,11 @@
 import django_filters
 from django_filters import FilterSet
+from rest_framework.exceptions import ValidationError
+
 from flowback.common.services import get_object
+from flowback.group.models import Group, GroupUser
 from flowback.user.models import User
+from backend.settings import env
 
 
 class UserFilter(FilterSet):
@@ -16,7 +20,14 @@ def get_user(user: int):
     return get_object(User, id=user)
 
 
-def user_list(*, filters=None):
+def user_list(*, fetched_by: User, filters=None):
+    # Block access to this api for members, unless group admin or higher.
+    if env('FLOWBACK_GROUP_ADMIN_USER_LIST_ACCESS_ONLY') \
+            and not Group.objects.filter(created_by=fetched_by).exists() \
+            and not GroupUser.objects.filter(user=fetched_by, is_admin=True) \
+            and not (fetched_by.is_staff and fetched_by.is_superuser):
+        raise ValidationError('User must be group admin or higher')
+
     filters = filters or {}
     qs = User.objects.all()
     return UserFilter(filters, qs).qs
