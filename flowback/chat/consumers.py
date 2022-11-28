51d4d94a372e368a5f4834f545782c67b0a991cf
gofven <cratedcreations@gmail.com>
2022-11-28 17:26:54 +0100

    Added new Websocket for Chat

--- flowback/chat/consumers.py
+++ flowback/chat/consumers.py
@@ -85,6 +85,104 @@ class GroupChatConsumer(AsyncWebsocketConsumer):
         obj.save()
 
 
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user = self.scope['user']
+
+        if self.scope['user'].is_anonymous:
+            await self.close()
+
+        # chat_id: user_<id:int>
+
+        user_groups = Group.objects.filter(groupuser__user__in=self.user).all()
+        self.chat_groups = [f'group_{x.id}' for x in user_groups] + [f'user_{self.user.id}']
+
+        # Join room group
+        for group in self.chat_groups:
+            await self.channel_layer.group_add(
+                group,
+                self.channel_name
+            )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        for group in self.chat_groups:
+            await self.channel_layer.group_discard(
+                group,
+                self.channel_name
+            )
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        class FilterSerializer(serializers.Serializer):
+            target_type = serializers.ChoiceField(('group', 'direct'))
+            message = serializers.CharField()
+            target = serializers.IntegerField()
+
+        class OutputSerializer(serializers.ModelSerializer):
+            class Meta:
+                model = User
+                fields = 'id', 'username', 'profile_image'
+
+        serializer = FilterSerializer(data=json.loads(text_data or '{}'))
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data
+
+        # Save message to database
+        if data.get('target_type') == 'direct':
+            await self.direct_message(**data)
+
+        else:
+            await self.group_message(**data)
+
+        # Send message to room group
+        await self.channel_layer.group_send(
+            await self.get_message_target(**data),
+            {
+                'type': 'chat_message',
+                'target_type': data.get('target_type'),
+                'user': OutputSerializer(self.user).data,
+                'message': data.get('message'),
+            }
+        )
+
+    # Receive message from room group
+    async def chat_message(self, content: dict):
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'message': content.get('message'),
+            'user': content.get('user')
+        }))
+
+    @database_sync_to_async
+    def get_message_target(self, target: int, target_type: str):
+        return f'user_{target}' if target_type == 'direct' else f'group_{target}'
+
+    @database_sync_to_async
+    def direct_message(self, *, message: str, target: int):
+        target = get_object(User, pk=target)
+
+        obj = DirectMessage(user=self.user,
+                            target_id=target.id,
+                            message=message)
+
+        obj.full_clean()
+        obj.save()
+
+    @database_sync_to_async
+    def group_message(self, message: str, target: int):
+        group_user = group_user_permissions(group=target, user=self.user.id)
+        obj = GroupMessage(group_user=group_user,
+                           message=message)
+
+        obj.full_clean()
+        obj.save()
+
+
+
 class DirectChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.user = self.scope['user']
@@ -159,3 +257,4 @@ class DirectChatConsumer(AsyncWebsocketConsumer):
 
         obj.full_clean()
         obj.save()
+
