64bc8c2492ff26e856772e0064b73756c1a3dc3e
gofven <cratedcreations@gmail.com>
2022-12-23 16:30:46 +0100

    Added notification unsubscribe

--- flowback/notification/urls.py
+++ flowback/notification/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import (NotificationListAPI,
                     NotificationMarkReadAPI,
+                    NotificationUnsubscribeAPI,
                     NotificationSubscriptionListAPI)
 
 
@@ -9,4 +10,5 @@ notification_patterns = [
     path('', NotificationListAPI.as_view(), name='notification_list'),
     path('subscriptions', NotificationSubscriptionListAPI.as_view(), name='notification_subscription_list'),
     path('read', NotificationMarkReadAPI.as_view(), name='notification_mark_read'),
+    path('unsubscribe', NotificationUnsubscribeAPI.as_view(), name='notification_unsubscribe')
 ]
