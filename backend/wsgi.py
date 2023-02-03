0a6ca32e1e5124052225cbfc4a2a5b7f7365fb28
gofven <cratedcreations@gmail.com>
2023-02-03 21:40:17 +0100

    Added FORCE_SCRIPT_NAME for subpath

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -13,21 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-_application = get_wsgi_application()
-
-
-def application(environ, start_response):
-    # http://flask.pocoo.org/snippets/35/
-    script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
-    print(script_name)
-    if script_name:
-        environ['SCRIPT_NAME'] = script_name
-        path_info = environ['PATH_INFO']
-        if path_info.startswith(script_name):
-            environ['PATH_INFO'] = path_info[len(script_name):]
-
-    scheme = environ.get('HTTP_X_SCHEME', '')
-    if scheme:
-        environ['wsgi.url_scheme'] = scheme
-
-    return _application(environ, start_response)
+application = get_wsgi_application()
