4407b21b04fac24ea82d33d0ac41bf0d0dbd0712
gofven <cratedcreations@gmail.com>
2023-03-08 18:06:39 +0100

    Updated notification list API URL Updated kanban list API URL

--- flowback/notification/urls.py
+++ flowback/notification/urls.py
@@ -7,7 +7,7 @@ from .views import (NotificationListAPI,
 
 
 notification_patterns = [
-    path('', NotificationListAPI.as_view(), name='notification_list'),
+    path('list', NotificationListAPI.as_view(), name='notification_list'),
     path('subscription', NotificationSubscriptionListAPI.as_view(), name='notification_subscription_list'),
     path('read', NotificationMarkReadAPI.as_view(), name='notification_mark_read'),
     path('unsubscribe', NotificationUnsubscribeAPI.as_view(), name='notification_unsubscribe')
