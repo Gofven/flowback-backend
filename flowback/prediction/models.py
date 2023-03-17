6c343dc97a5c130b1fd6cbcd4e4a4b8de54333b2
gofven <cratedcreations@gmail.com>
2023-03-17 16:50:13 +0100

    Added prediction models.py

--- flowback/prediction/models.py
+++ flowback/prediction/models.py
@@ -1,3 +1,41 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+from flowback.common.models import BaseModel
+
+
+class PredictionStatement(BaseModel):
+    description = models.TextField(max_length=2000)
+    end_date = models.DateTimeField()
+    # created_by: represents ownership
+    # fk: represents relationship
+
+    class Meta:
+        abstract = True
+
+
+class PredictionStatementSegment(BaseModel):
+    is_true = models.BooleanField()
+    # prediction_statement: represents prediction statement
+    # fk: represents relationship
+
+    class Meta:
+        abstract = True
+
+
+class PredictionStatementVote(BaseModel):
+    vote = models.BooleanField()
+    # prediction_statement: represents prediction statement
+    # created_by: represents ownership
+
+    class Meta:
+        abstract = True
+
+
+class Prediction(BaseModel):
+    score = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(0)])
+    # prediction_statement: represents prediction statement
+    # created_by: represents ownership
+
+    class Meta:
+        abstract = True
