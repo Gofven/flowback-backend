8a00b90f34d65c9669abe2241be84dde3c38de37
gofven <cratedcreations@gmail.com>
2023-03-29 18:28:37 +0200

    Updated prediction selectors

--- flowback/poll/selectors/prediction.py
+++ flowback/poll/selectors/prediction.py
@@ -1,13 +1,11 @@
-from typing import Union
-
 import django_filters
 from django.db import models
-from django.db.models import Avg, When, F, Case, Count, Q
+from django.db.models import Avg, When, F, Case, Count, Q, Exists, Subquery, OuterRef
 from django.db.models.lookups import LessThan
 from django.utils import timezone
 
 from flowback.group.selectors import group_user_permissions
-from flowback.poll.models import PollPredictionStatement, PollPrediction
+from flowback.poll.models import PollPredictionStatement, PollPrediction, PollPredictionStatementVote
 from flowback.user.models import User
 
 
@@ -15,6 +13,8 @@ class BasePollPredictionStatementFilter(django_filters.FilterSet):
     proposals = django_filters.NumberFilter(field_name='pollpredictionstatementsegment__proposal', lookup_expr='in')
     description = django_filters.CharFilter(lookup_expr='icontains')
     created_by_id = django_filters.NumberFilter(field_name='created_by__user_id', lookup_expr='exact')
+    user_prediction_exists = django_filters.BooleanFilter(lookup_expr='isnull', exclude=True)
+    user_vote_exists = django_filters.BooleanFilter(lookup_expr='isnull', exclude=True)
 
 
     class Meta:
@@ -25,18 +25,27 @@ class BasePollPredictionStatementFilter(django_filters.FilterSet):
 
 def poll_prediction_statement_list(*, fetched_by: User, group_id: int, filters=None):
     filters = filters or {}
-    group_user_permissions(group=group_id, user=fetched_by)
+    group_user = group_user_permissions(group=group_id, user=fetched_by)
 
-    # TODO Add user_prediction and user_vote
-    qs = PollPredictionStatement.objects.filter(poll__created_by__group_id=group_id,
-                                       ).annotate(score=Case(When(LessThan(F('end_date'), timezone.now()),
-                                                             then=Avg('pollprediction__score')),
-                                                             default=-1, output_field=models.FloatField),
-                                                  vote_yes=Count(F('pollpredictionstatementvote'),
-                                                                 filter=Q(pollpredictionstatementvote__vote=True)),
-                                                  vote_no=Count(F('pollpredictionstatementvote'),
-                                                                filter=Q(pollpredictionstatementvote__vote=False))
-                                                  ).all()
+    # Annotations
+    score = Case(When(LessThan(F('end_date'), timezone.now()),
+                 then=Avg('pollprediction__score')),
+                 default=None, output_field=models.FloatField)
+    vote_yes = Count(F('pollpredictionstatementvote'), filter=Q(pollpredictionstatementvote__vote=True))
+    vote_no = Count(F('pollpredictionstatementvote'), filter=Q(pollpredictionstatementvote__vote=False))
+    user_prediction = Subquery(PollPrediction.objects.filter(prediction_statement=OuterRef('pk'),
+                                                             created_by=fetched_by).first())
+    user_vote = Subquery(PollPredictionStatementVote.objects.filter(prediction_statement=OuterRef('pk'),
+                                                                    created_by=fetched_by).first())
+
+    qs = PollPredictionStatement.objects.filter(poll__created_by__group_id=group_id
+                                                ).annotate(score=score,
+                                                           vote_yes=vote_yes,
+                                                           vote_no=vote_no,
+                                                           user_prediction=user_prediction,
+                                                           user_vote=user_vote
+                                                           ).all()
+
     return BasePollPredictionStatementFilter(filters, qs).qs
 
 
