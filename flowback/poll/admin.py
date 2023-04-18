2f8d90afbd2bf73927dd76d7e533459ec602bf86
gofven <cratedcreations@gmail.com>
2023-04-18 16:06:25 +0200

    Added poll admin.py

--- flowback/poll/admin.py
+++ flowback/poll/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Poll
 
-# Register your models here.
+@admin.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+    list_display = ('title', 'poll_type', 'active', 'public', 'start_date', 'end_date')
+    list_filter = ('poll_type', 'active', 'public')
+    search_fields = ('title', 'description')
+    date_hierarchy = 'start_date'
+    fieldsets = (
+        (None, {
+            'fields': ('created_by', 'title', 'description', 'poll_type', 'tag')
+        }),
+        ('Visibility', {
+            'fields': ('active', 'public')
+        }),
+        ('Dates', {
+            'fields': ('start_date', 'proposal_end_date', 'vote_start_date', 'delegate_vote_end_date', 'vote_end_date', 'end_date')
+        }),
+        ('Optional Dynamic Counting Support', {
+            'fields': ('dynamic',)
+        }),
+    )
+    ordering = ('-created_by', 'created_by')
