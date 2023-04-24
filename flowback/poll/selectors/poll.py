fedc1f8369c331688b17c65b51f9755c6600ebbf
gofven <cratedcreations@gmail.com>
2023-04-24 19:47:19 +0200

    Changed poll list order_by to use start_date instead of created_by

--- flowback/poll/selectors/poll.py
+++ flowback/poll/selectors/poll.py
@@ -12,8 +12,8 @@ from flowback.group.selectors import group_user_permissions
 
 
 class BasePollFilter(django_filters.FilterSet):
-    order_by = django_filters.OrderingFilter(fields=(('created_at', 'created_at_asc'),
-                                                     ('-created_at', 'created_at_desc')))
+    order_by = django_filters.OrderingFilter(fields=(('start_date', 'start_date_asc'),
+                                                     ('-start_date', 'start_date_desc')))
     start_date = django_filters.DateFromToRangeFilter()
     end_date = django_filters.DateFromToRangeFilter()
     tag_name = django_filters.CharFilter(lookup_expr=['exact', 'icontains'], field_name='tag__name')
