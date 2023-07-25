2578e320b2ef8a76f56fcdb3d0873938818c0d2e
gofven <cratedcreations@gmail.com>
2023-07-25 14:22:19 +0200

    Updated admin comment search field

--- flowback/comment/admin.py
+++ flowback/comment/admin.py
@@ -5,6 +5,6 @@ from .models import Comment
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('comment_section', 'author', 'message', 'edited', 'active', 'parent', 'score', 'created_at', 'updated_at')
     list_filter = ('edited', 'active', 'score')
-    search_fields = ('author__email__contains',)
+    search_fields = ('author__email__icontains',)
     date_hierarchy = 'created_at'
     ordering = ('-author', 'author')
