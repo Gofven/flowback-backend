83bdce3130ecc992e7897fed775aa596fc03b140
gofven <cratedcreations@gmail.com>
2024-01-26 13:40:36 +0100

    Added message channel topic create service

--- flowback/chat/services.py
+++ flowback/chat/services.py
@@ -2,7 +2,8 @@ from django.shortcuts import get_object_or_404
 from django.utils.datetime_safe import datetime
 from rest_framework.exceptions import ValidationError
 
-from flowback.chat.models import MessageChannel, Message, MessageChannelParticipant, MessageFileCollection
+from flowback.chat.models import MessageChannel, Message, MessageChannelParticipant, MessageFileCollection, \
+    MessageChannelTopic
 from flowback.common.services import get_object, model_update
 from flowback.files.models import FileCollection
 from flowback.files.services import upload_collection
@@ -129,3 +130,11 @@ def leave_message_channel(*, user_id: int, channel_id: int):
     participant = get_object(MessageChannelParticipant, user=user, channel=channel)
 
     participant.delete()
+
+
+def message_channel_topic_create(*, channel_id: int, topic_name: str):
+    channel = get_object(MessageChannel, id=channel_id)
+
+    topic = MessageChannelTopic(name=topic_name)
+    topic.full_clean()
+    topic.save()
