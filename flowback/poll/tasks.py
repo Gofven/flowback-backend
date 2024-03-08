5b661bb959785f57f548e77b4778e4bf31cccbe8
gofven <cratedcreations@gmail.com>
2024-03-08 17:31:19 +0100

    wip poll_prediction_bet_count

--- flowback/poll/tasks.py
+++ flowback/poll/tasks.py
@@ -1,9 +1,12 @@
+import random
 from celery import shared_task
-from django.db.models import Count, Q
+from django.db.models import Count, Q, Sum, OuterRef, Case, When, F, Subquery
+from django.utils import timezone
 
 from flowback.common.services import get_object
-from flowback.group.models import GroupTags
-from flowback.poll.models import Poll, PollAreaStatement, PollPredictionBet
+from flowback.group.models import GroupTags, GroupUser
+from flowback.poll.models import Poll, PollAreaStatement, PollPredictionBet, PollPredictionStatementVote, \
+    PollPredictionStatement
 
 import numpy as np
 
@@ -28,69 +31,114 @@ def poll_area_vote_count(poll_id: int):
 
 @shared_task
 def poll_prediction_bet_count(poll_id: int):
+    # For one prediction, assuming no bias and stationary predictors
+    history_limit = 100
+
     # Get every predictor participating in poll
+    timestamp = timezone.now()  # Avoid new bets causing list to be offset
+    poll = get_object(Poll, id=poll_id)
+    predictors = GroupUser.objects.filter(pollpredictionbet__poll=poll).all()
+
+    # Get outcomes
+    outcome_subquery = PollPredictionStatementVote.objects.filter(prediction_statement_id=OuterRef('id'),
+                                                                  created_at__lte=timestamp
+                                                                  ).aggregate(outcome=Sum('vote')
+                                                                              ).values('outcome')
+
+    # Get list of previous outcomes in a given area (poll)
+    outcomes = PollPredictionStatement.objects.filter(poll__tag=poll.tag,
+                                                      poll__end_date__lte=timestamp,
+                                                      created_at__lte=timestamp,
+                                                      ).annotate(outcome=Subquery(outcome_subquery)
+                                                                 ).order_by('-created_at').all()
+
+    predictor_bets = []
+    current_bets = []
+    for predictor in predictors:
+        bet_subquery = PollPredictionBet.objects.filter(created_by=predictor,
+                                                        prediction_statement_id=OuterRef('id')).values('score')
+
+        bets = PollPredictionStatement.objects.filter(poll__tag=poll.tag,
+                                                      poll__end_date__lte=timestamp,
+                                                      created_at__lte=timestamp
+                                                      ).annotate(bet=Subquery(bet_subquery)
+                                                                 ).values_list('bet_subquery', flat=True)
+
+        # TODO get all current_bets for every statement in poll, bets needs to be counted once
+
+        predictor_bets.append(bets)
+
+    # Get bets
 
     # Get list of bets in the given poll, ordered
     #   - if any bet missing, dismiss count
-    PollPredictionBet.objects.filter(prediction_statement__poll_id=poll_id)
 
-    # Get list of previous outcomes in a given area (poll)
+    # Small decimal (AT LEAST a magnitude below 10^(-6))
+    small_decimal = 10 ** -7
 
-    # For one prediction, assuming no bias and stationary predictors
+    # Current bets by each predictor for one given statement, in order
+    #   (first equal to predictor 1 bets, 2 to 2 bets etc.)
+    # IMPORTANT: do not append the current bet until AFTER the combined bet has been calculated
+    #   and saved and there is an outcome
+    current_bets = np.array([[0.99], [0.9]])
 
+    # Create lists of predictor bets in order (matching outcomes, None for missing),
+    #   don't include ongoing bets (max 100)
+    # If the determinant of a predictor bets list is zero then set the first value to the smallest non zero value
+    # TODO future test combinations of values
+    predictor_bets = [[0, 1, 0.7, 0, 1], [0, 0.2, 1, 0, 0.8]]
 
+    # Calculation below
 
+    predictor_errors = []
+    for bets in predictor_bets:
+        predictor_errors.append(np.array([outcomes[i] - bets[i]
+                                          if bets[i] is not None else None for i in range(len(outcomes))]))
 
-    # TODO solve how to handle missing prediction bets
-    # The following two comments is a possible solution that inserts nan into the matrix
-    # import numpy.ma as ma
-    # cv = ma.cov(ma.masked_invalid(my_matrix), rowvar=False)
-    # Otherwise, the standard solution is to skip the value for all predictors
-
-    # In a given area
-    outcomes = [0, 1, 1, 0, 1]
-
-    # Current bets by each predictor in order
-    current_bets = np.array([[0.5], [0.5]])
-
-    # Constructing the predictor error matrix
-    predictor_1_bets = [0, 0.4, 0.6, 0, 1]
-    predictor_2_bets = [0, 0.4, 0.4, 0, 1]
-    predictor_1_errors = np.array([outcomes[i] - predictor_1_bets[i] for i in range(len(outcomes))])
-    predictor_2_errors = np.array([outcomes[i] - predictor_2_bets[i] for i in range(len(outcomes))])
-
-    predictor_errors = [predictor_1_errors, predictor_2_errors]
-
-    predictor_error_matrix = np.array([predictor_1_errors, predictor_2_errors])
-
-    #If a predictor has not bet on a certain prediction then
-    # their bet will be None for said prediction
+    # If a predictor has not bet on a certain prediction then their bet will be None for said prediction
     def drop_incomparable_values(arr_1, arr_2):
+        drop_list = []
+
         for i in range(len(arr_1)):
-            if arr_1[i] or arr_2[i] is None:
-                arr_1.pop(i)
-                arr_2.pop(i)
+            if arr_1[i] is None or arr_2[i] is None:
+                drop_list.append(i)
+
+        arr_1 = np.delete(arr_1, drop_list)
+        arr_2 = np.delete(arr_2, drop_list)
+
         return arr_1, arr_2
 
     def covariance(arr_1, arr_2):
-        covariance_array = [(arr_1[i] - np.mean(arr_1))*(arr_2[i] - np.mean(arr_2)) for i in range(len(arr_1))]
-        return (1/len(arr_1))*sum(covariance_array)
+        covariance_array = [(arr_1[i] - np.mean(arr_1)) * (arr_2[i] - np.mean(arr_2)) for i in range(len(arr_1))]
+        return (1 / len(arr_1)) * sum(covariance_array)
 
-    covariance_matrix_1 = []
+    covariance_matrix = []
     for i in range(len(predictor_errors)):
         row = []
+
         for j in range(len(predictor_errors)):
             comparable_errors = drop_incomparable_values(predictor_errors[i], predictor_errors[j])
             row.append(covariance(comparable_errors[0], comparable_errors[1]))
-        covariance_matrix_1.append(row)
 
-    print(covariance_matrix_1)
+        covariance_matrix.append(row)
 
-    # Code below is checked and correct
+    np_covariance_matrix = np.array(covariance_matrix)
 
-    covariance_matrix = np.cov(predictor_error_matrix, ddof=0)
+    # The inverse only exists when the determinant is non-zero, this can be made sure of by changing small decimals
+    if np.linalg.det(np_covariance_matrix) == 0:
+        determinant_is_zero = True
+        print("Zero determinant")
 
-    inverse_covariance_matrix = np.linalg.inv(covariance_matrix)
+        while determinant_is_zero:
+            for i in range(np_covariance_matrix.shape[0]):
+                for j in range(np_covariance_matrix.shape[0]):
+                    np_covariance_matrix[i][j] += small_decimal * [-1, 1][random.randint(0, 1)]
+
+            det = np.linalg.det(np_covariance_matrix)
+            if det != 0:
+                determinant_is_zero = False
+
+    inverse_covariance_matrix = np.linalg.inv(np_covariance_matrix)
 
     column_one_vector = np.array([[1]] * inverse_covariance_matrix.shape[0])
     row_one_vector = np.array([1] * inverse_covariance_matrix.shape[0])
@@ -103,7 +151,11 @@ def poll_prediction_bet_count(poll_id: int):
     bet_weights = nominator * (1 / denominator)
     transposed_bet_weights = np.transpose(bet_weights)
 
-    combined_bet = np.matmul(transposed_bet_weights, current_bets)
+    combined_bet = float(np.matmul(transposed_bet_weights, current_bets)[0][0])
+
+    # Sanity check
+    check = np.matmul(transposed_bet_weights, row_one_vector)
+    if (check[0] > 1 + small_decimal) or (0.99 + small_decimal > check[0]):
+        print(f"Error with weights: {check[0]:.4f}")
 
     print(combined_bet)
-
