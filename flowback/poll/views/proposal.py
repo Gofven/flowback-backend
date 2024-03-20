4b1fbdd86eb80fa1d57e5a21bbadde877e1e2087
gofven <cratedcreations@gmail.com>
2024-03-21 00:59:01 +0100

    Fixed proposal order_by is not optional

--- flowback/poll/views/proposal.py
+++ flowback/poll/views/proposal.py
@@ -22,7 +22,9 @@ class PollProposalListAPI(APIView):
         default_limit = 10
 
     class FilterSerializer(serializers.Serializer):
-        order_by = serializers.ChoiceField(choices=['created_at_asc', 'created_at_desc',
+        order_by = serializers.ChoiceField(required=False,
+                                           default='created_at_desc',
+                                           choices=['created_at_asc', 'created_at_desc',
                                                     'score_asc', 'score_desc'])
         id = serializers.IntegerField(required=False)
         created_by_user_id_list = serializers.CharField(required=False)
