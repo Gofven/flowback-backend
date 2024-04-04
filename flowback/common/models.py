761718e8d43f71383bd857b8e14e27ef8f17c6fa
Gofven <cratedcreations@gmail.com>
2024-04-04 16:57:56 +0200

    Added generate_exclusive_query helper function

--- flowback/common/models.py
+++ flowback/common/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from pgtrigger import Q
 
 
 class BaseModel(models.Model):
@@ -8,3 +9,18 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
+
+
+# Generates a query where only one of the fields is allowed to be set, while all other fields must be null
+def generate_exclusive_query(fields: list[str]) -> Q:
+    queryset_merge = None
+
+    for i in range(len(fields)):
+        queryset_slice = None
+
+        for j, field in enumerate(fields):
+            queryset_slice &= Q(**{field: i == j})
+
+        queryset_merge |= queryset_slice
+
+    return Q(queryset_merge)
