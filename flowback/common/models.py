5e3e3e77cc61b9e7ee3096845539986c7730031b
Gofven <cratedcreations@gmail.com>
2024-04-09 17:24:43 +0200

    Added generate_exclusive_constraint_eq helper

--- flowback/common/models.py
+++ flowback/common/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import When, Case
 from django.utils import timezone
 from pgtrigger import Q
 
@@ -24,3 +25,20 @@ def generate_exclusive_q(*fields: str) -> Q:
         queryset_merge |= queryset_slice
 
     return Q(queryset_merge)
+
+
+def generate_exclusive_constraint_eq(*, base: str, target: str, fields: tuple[tuple[str, str]]) -> Case:
+    """
+    Generates Case of exclusive constraint query for given fields, allows for checking if e.g. user is in the
+    same group as other fields
+    :type base: str, the base route to the exclusive model reference
+    :type target: str, the absolute route to the target to compare
+    :type fields: tuple[tuple[str, str]], list of fields for each exclusive constraint, appended with the base field.
+                  First value is to check if it's null, second is to check if it's equal to the target
+    """
+    constraints = []
+    for exist_field, compare_field in fields:
+        constraints.append(When(Q(**{f"{base}__{exist_field}": False}),
+                                then=Q(**{f"{base}__{compare_field}": compare_field})))
+
+    return Case(*constraints, default=False)
