aeeb538fb91762f13e880cd9b1896d7e5df4152b
Gofven <cratedcreations@gmail.com>
2024-04-19 15:23:50 +0200

    Fix to previous commit

--- flowback/poll/services/proposal.py
+++ flowback/poll/services/proposal.py
@@ -45,8 +45,13 @@ def poll_proposal_create(*, user_id: int, poll_id: int,
                               end_date=data['end_date'],
                               origin_name=PollProposal.schedule_origin,
                               origin_id=proposal.id)
+        try:
+            event.full_clean()
 
-        event.full_clean()
+        except ValidationError as e:
+            proposal.delete()
+            raise e
+
         event.save()
 
         schedule_proposal = PollProposalTypeSchedule(proposal=proposal,
