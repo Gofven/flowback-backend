e3892625c6b39f03ff3aebbea8cc907d043fbf45
Gofven <cratedcreations@gmail.com>
2024-04-23 16:40:23 +0200

    Fixed previous bug

--- flowback/poll/models.py
+++ flowback/poll/models.py
@@ -360,7 +360,7 @@ class PollPredictionStatement(PredictionStatement):
     poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
 
     def clean(self):
-        if self.poll.end_date < self.end_date:
+        if self.poll.end_date > self.end_date:
             raise ValidationError('Poll ends later than prediction statement deadline')
 
     @receiver(post_delete, sender=PollProposal)
