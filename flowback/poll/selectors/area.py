9708ee07055d3d0e8f1eca9abb49640ece46a30b
Gofven <cratedcreations@gmail.com>
2024-07-04 16:27:35 +0200

    Renamed vote field to user_vote (#84)

--- flowback/poll/selectors/area.py
+++ flowback/poll/selectors/area.py
@@ -14,7 +14,7 @@ class BasePollAreaStatementFilter(django_filters.FilterSet):
                                                      ('score', 'score_asc'),
                                                      ('-score', 'score_desc')))
 
-    vote = django_filters.BooleanFilter()
+    user_vote = django_filters.BooleanFilter()
     tag = django_filters.CharFilter(field_name='pollareastatementsegment__tag__name',
                                     lookup_expr='iexact')
 
@@ -24,9 +24,9 @@ def poll_area_statement_list(*, user: User, poll_id: int, filters=None):
     poll = get_object(Poll, id=poll_id)
     group_user = group_user_permissions(user=user, group=poll.created_by.group)
 
-    user_votes = PollAreaStatementVote.objects.filter(created_by=group_user,
-                                                      poll_area_statement=OuterRef('pk')).values('vote')
-    qs = PollAreaStatement.objects.filter(poll=poll).annotate(vote=Subquery(user_votes,
-                                                                            output_field=models.BooleanField()))
+    user_vote = PollAreaStatementVote.objects.filter(created_by=group_user,
+                                                     poll_area_statement=OuterRef('pk')).values('vote')
+    qs = PollAreaStatement.objects.filter(poll=poll).annotate(user_vote=Subquery(user_vote,
+                                                                                 output_field=models.BooleanField()))
 
     return BasePollAreaStatementFilter(filters, qs).qs
