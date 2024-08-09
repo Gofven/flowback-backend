cbaf715ff2b09f9724de04a51fa88faa7d1069e9
Gofven <cratedcreations@gmail.com>
2024-08-09 10:56:40 +0200

    Added mail alias for email

--- backend/oidc_provider_settings.py
+++ backend/oidc_provider_settings.py
@@ -5,9 +5,11 @@ def userinfo(claims, user: User):
     # Populate claims dict.
     claims['name'] = user.username
     claims['preferred_username'] = user.username
+
     claims['picture'] = user.profile_image
     claims['website'] = user.website
 
     claims['email'] = user.email
+    claims['mail'] = user.email
 
     return claims
