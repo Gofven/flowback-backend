01cb2967561a9d08175b0eda812308890ee4f1ad
Gofven <cratedcreations@gmail.com>
2024-08-15 15:59:21 +0200

    Updated OIDC userinfo

--- backend/oidc_provider_settings.py
+++ backend/oidc_provider_settings.py
@@ -6,7 +6,7 @@ def userinfo(claims, user: User):
     claims['name'] = user.username
     claims['preferred_username'] = user.username
 
-    claims['picture'] = user.profile_image
+    claims['picture'] = user.profile_image.url
     claims['website'] = user.website
 
     claims['email'] = user.email
