57c2800eb70a0f1e16e40e4a6aab9b073ca4c007
Gofven <cratedcreations@gmail.com>
2024-08-29 12:36:11 +0200

    Added Poll model to admin

--- flowback/poll/admin.py
+++ flowback/poll/admin.py
@@ -1,64 +1,15 @@
 from django.contrib import admin
-from .models import Poll, PollProposal, PollPredictionBet, PollPhaseTemplate
+
+from flowback.poll.models import Poll
 
 
+# Register your models here.
 @admin.register(Poll)
 class PollAdmin(admin.ModelAdmin):
-    list_display = ('title', 'poll_type', 'active', 'public', 'start_date', 'end_date')
-    list_filter = ('poll_type', 'active', 'public')
-    search_fields = ('title', 'description')
-    date_hierarchy = 'start_date'
-    fieldsets = (
-        (None, {
-            'fields': ('created_by', 'title', 'description', 'poll_type', 'tag')
-        }),
-        ('Visibility', {
-            'fields': ('active', 'public')
-        }),
-        ('Dates', {
-            'fields': (
-            'start_date', 'proposal_end_date', 'vote_start_date', 'delegate_vote_end_date', 'vote_end_date', 'end_date')
-        }),
-        ('Optional Dynamic Counting Support', {
-            'fields': ('dynamic',)
-        }),
-    )
-    ordering = ('-created_by', 'created_by')
+    list_display = ('id', 'created_by', 'title', 'poll_type', 'status')
 
-
-@admin.register(PollProposal)
-class PollProposalAdmin(admin.ModelAdmin):
-    list_display = ('poll', 'title', 'description', 'score', 'created_by')
-
-
-@admin.register(PollPredictionBet)
-class PollPredictionAdmin(admin.ModelAdmin):
-    list_display = ('prediction_statement', 'created_by')
-
-
-@admin.register(PollPhaseTemplate)
-class PollPhaseTemplateAdmin(admin.ModelAdmin):
-    list_display = ('name',
-                    'poll_type',
-                    'poll_is_dynamic',
-                    'created_by_group_user')
-
-    list_filter = ('created_by_group_user', 'poll_type', 'poll_is_dynamic')
-    search_fields = ('name',)
-    date_hierarchy = 'created_at'
-
-    fieldsets = (
-        ('General', dict(fields=['created_by_group_user',
-                                 'poll_type',
-                                 'poll_is_dynamic'])),
-
-        ('Delta', dict(fields=['area_vote_time_delta'
-                               'proposal_time_delta',
-                               'prediction_statement_time_delta',
-                               'prediction_bet_time_delta',
-                               'delegate_vote_time_delta',
-                               'vote_time_delta',
-                               'end_time_delta']))
-    )
-
-    ordering = ('-created_at', 'created_at')
+    fieldsets = [('General Information', {'fields': ['created_by', 'title', 'description', 'poll_type',
+                                                     'dynamic', 'quorum', 'phases']}),
+                 ('Visibility', {'fields': ['pinned', 'active', 'public']}),
+                 ('Advanced options', {'fields': ['attachments', 'blockchain_id', 'allow_fast_forward']}),
+                 ('Debug', {'fields': ['status', 'tag']})]
