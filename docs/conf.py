fadf28cf0ce8e9069221b1ee53f697a966201a16
Gofven <cratedcreations@gmail.com>
2024-10-14 12:19:28 +0200

    Test ReadTheDocs

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ import django
 from datetime import date
 
 sys.path.insert(0, os.path.abspath(".."))
-os.environ["DJANGO_SETTINGS_MODULE"] = "docs.django_settings"
+os.environ["DJANGO_SETTINGS_MODULE"] = "backend.settings"
 django.setup()
 
 project = "Flowback"
