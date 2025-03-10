4fcea25999816c7419ccd62a549f94c55e88e935
Gofven <31272508+Gofven@users.noreply.github.com>
2025-03-10 17:23:29 +0100

    Updated migrations

--- flowback/group/migrations/0045_workgroup_chat_workgroupuser_active_and_more.py
+++ flowback/group/migrations/0045_workgroup_chat_workgroupuser_active_and_more.py
@@ -7,26 +7,19 @@ import django.db.models.deletion
 def pre_populate_fields(apps, schema_editor):
     MessageChannel = apps.get_model('chat', 'messagechannel')
     WorkGroup = apps.get_model('group', 'workgroup')
-    work_groups = []
 
     for work_group in WorkGroup.objects.all():
         work_group.chat = MessageChannel.objects.create(origin_name='workgroup', title=work_group.name)
-        work_groups.append(work_group)
-
-    WorkGroup.objects.bulk_update(work_groups, ['chat'])
+        work_group.save()
 
 
 def pre_populate_fields_two(apps, schema_editor):
     MessageChannelParticipant = apps.get_model('chat', 'messagechannelparticipant')
     WorkGroupUser = apps.get_model('group', 'workgroupuser')
-    work_group_users = []
 
     for work_group_user in WorkGroupUser.objects.all():
         work_group_user.chat_participant = MessageChannelParticipant.objects.create(channel=work_group_user.work_group.chat, user=work_group_user.group_user.user)
-        work_group_users.append(work_group_user)
-
-    WorkGroupUser.objects.bulk_update(work_group_users, ['chat_participant'])
-
+        work_group_user.save()
 
 class Migration(migrations.Migration):
 
@@ -39,7 +32,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='workgroup',
             name='chat',
-            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.PROTECT, to='chat.messagechannel', null=True)
+            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, to='chat.messagechannel', null=True)
         ),
         migrations.RunPython(pre_populate_fields),
         migrations.AlterField(
@@ -49,15 +42,16 @@ class Migration(migrations.Migration):
         ),
         migrations.AddField(
             model_name='workgroupuser',
+            name='chat_participant',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, to='chat.messagechannelparticipant',
+                                    null=True)
+        ),
+        migrations.RunPython(pre_populate_fields_two),
+        migrations.AddField(
+            model_name='workgroupuser',
             name='active',
             field=models.BooleanField(default=True),
         ),
-        migrations.AddField(
-            model_name='workgroupuser',
-            name='chat_participant',
-            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.PROTECT, to='chat.messagechannelparticipant', null=True)
-        ),
-        migrations.RunPython(pre_populate_fields_two),
         migrations.AlterField(
             model_name='workgroupuser',
             name='chat_participant',
