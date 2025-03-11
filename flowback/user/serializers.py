3ced5f7d0cd70d8778af6e2ce3cc2ef276bd529d
Gofven <31272508+Gofven@users.noreply.github.com>
2025-03-11 19:49:33 +0100

    Replaced direct_message with chat_status (#190)

--- flowback/user/serializers.py
+++ flowback/user/serializers.py
@@ -6,4 +6,4 @@ from flowback.user.models import User
 class BasicUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'profile_image', 'banner_image')
+        fields = ('id', 'username', 'profile_image', 'banner_image', 'public_status', 'chat_status')
