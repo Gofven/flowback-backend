e67960852e0c5774a463ca316e2b14ab1121e621
Gofven <31272508+Gofven@users.noreply.github.com>
2025-03-18 16:54:51 +0100

    Updated notification models

--- flowback/notification/admin.py
+++ flowback/notification/admin.py
@@ -1,21 +1,22 @@
 from django.contrib import admin
 from .models import Notification, NotificationChannel, NotificationObject, NotificationSubscription
 
-@admin.register(Notification)
-class NotificationAdmin(admin.ModelAdmin):
-    list_display = ('user', 'notification_object', 'read')
-
 
 @admin.register(NotificationChannel)
 class NotificationChannelAdmin(admin.ModelAdmin):
-    list_display = ('category', 'sender_type', 'sender_id')
+    list_display = ('name',)
 
 
 @admin.register(NotificationObject)
 class NotificationObjectAdmin(admin.ModelAdmin):
-    list_display = ('related_id', 'action', 'message', 'timestamp', 'channel')
+    list_display = ('action', 'message', 'category', 'data', 'timestamp', 'channel')
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('subscriber', 'notification_object', 'read')
 
 
 @admin.register(NotificationSubscription)
 class NotificationSubscriptionAdmin(admin.ModelAdmin):
-    list_display = ('user', 'channel')
+    list_display = ('user', 'channel', 'categories')
--- flowback/notification/models.py
+++ flowback/notification/models.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.utils import timezone
 
@@ -6,8 +7,7 @@ from flowback.user.models import User
 
 
 class NotificationChannel(BaseModel):
-    users = models.ManyToManyField(User, through='notification.NotificationSubscription')
-    origin = models.CharField(max_length=255, help_text='Origin of the channel, preferably name of the parent model')
+    name = models.CharField(default='default', max_length=255, help_text='Origin of the channel, preferably name of the parent model')
 
 
 # NotificationObject is created containing data for each occurrence
@@ -22,35 +22,38 @@ class NotificationObject(BaseModel):
 
     action = models.CharField(choices=Action.choices)
     message = models.TextField(max_length=2000)
-    category = models.CharField(max_length=255, default='main', help_text='Category of the notification')
+    category = models.CharField(max_length=255, default='default', help_text='Category of the notification')
     data = models.JSONField(null=True, blank=True)  # Suggested to store relevant data for user
     timestamp = models.DateTimeField(default=timezone.now)
     channel = models.ForeignKey(NotificationChannel, on_delete=models.CASCADE)
 
-    notifications = models.ManyToManyField(User, through='notification.Notification')
+    notifications = models.ManyToManyField('notification.Notification')
 
-    def post_save(self, created, *args, **kwargs):
+    @classmethod
+    def post_save(cls, instance, created, *args, **kwargs):
         if created:
-            users = NotificationSubscription.objects.filter(channel=self.channel).values('user')
-            notifications = [Notification(user=x, notification_object=self) for x in users]
+            users = NotificationSubscription.objects.filter(channel=instance.channel,
+                                                            categories__in=instance.category).values('user')
+            notifications = [Notification(user=x, notification_object=instance) for x in users]
             Notification.objects.bulk_create(notifications, ignore_conflicts=True)
 
 
 # Notification is created for every user subscribed to a channel,
 # with a NotificationObject attached to it containing the data
 class Notification(BaseModel):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subscriber = models.OneToOneField('notification.NotificationSubscription', on_delete=models.CASCADE)
     notification_object = models.ForeignKey("notification.NotificationObject", on_delete=models.CASCADE)
     read = models.BooleanField(default=False)
 
     class Meta:
-        unique_together = ('user', 'notification_object')
+        unique_together = ('subscriber', 'notification_object')
 
 
 # Notification Subscription allows users to subscribe to the NotificationChannel, to get Notifications for themselves
 class NotificationSubscription(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     channel = models.ForeignKey(NotificationChannel, on_delete=models.CASCADE)
+    categories = ArrayField(models.CharField(max_length=255))
 
     class Meta:
         unique_together = ('user', 'channel')
