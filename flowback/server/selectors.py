6911d1f73df1107bef46607dec7c95d194833482
Gofven <31272508+Gofven@users.noreply.github.com>
2025-04-11 16:00:05 +0200

    Updated reports_list

--- flowback/server/selectors.py
+++ flowback/server/selectors.py
@@ -4,7 +4,7 @@ from flowback.user.models import User, Report
 
 
 def reports_list(fetched_by: User):
-    if not fetched_by.is_staff or fetched_by.is_superuser:
+    if not (fetched_by.is_staff or fetched_by.is_superuser):
         raise PermissionDenied('Only server staff members can view reports')
 
     return Report.objects.all().order_by('-created_at')
