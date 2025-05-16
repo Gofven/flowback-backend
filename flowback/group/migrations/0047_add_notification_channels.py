61603ac4842d76a00720c9838b37169bce3874a3
Gofven <31272508+Gofven@users.noreply.github.com>
2025-05-16 17:22:36 +0200

    Added custom group migration

---
+++ flowback/group/migrations/0047_add_notification_channels.py
@@ -0,0 +1,18 @@
+from django.db import migrations
+
+def add_notification_channels(apps, schema_editor):
+    NotificationChannel = apps.get_model('notification', 'NotificationChannel')
+    Group = apps.get_model('group', 'Group')
+
+    for group in Group.objects.all():
+        NotificationChannel.objects.get_or_create(content_object=group)
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('group', '0046_alter_workgroup_chat_and_more'),
+    ]
+
+    operations = [
+        migrations.RunPython(add_notification_channels)
+    ]
