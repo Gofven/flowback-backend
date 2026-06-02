b77e863b97e9253b0cb0dd71e9dc25cb12ffbfc9
Gofven <31272508+Gofven@users.noreply.github.com>
2026-06-02 15:09:03 +0200

    Updated test_user_home_feed_visibility

--- flowback/user/tests/test_user.py
+++ flowback/user/tests/test_user.py
@@ -274,7 +274,7 @@ class UserTest(APITestCase):
         ## Admin
         response = generate_request(api=UserHomeFeedAPI, user=group_user_private_admin.user)
         self.assertEqual(response.status_code, 200, response.data)
-        self.assertEqual(response.data['count'], 25)
+        self.assertEqual(response.data['count'], 20)
 
         ## WorkGroup User
         response = generate_request(api=UserHomeFeedAPI, user=group_user_private_workgroupuser.group_user.user)
@@ -291,7 +291,7 @@ class UserTest(APITestCase):
         ## Admin
         response = generate_request(api=UserHomeFeedAPI, user=group_user_public_admin.user)
         self.assertEqual(response.status_code, 200, response.data)
-        self.assertEqual(response.data['count'], 15)
+        self.assertEqual(response.data['count'], 10)
 
         ## WorkGroup User
         response = generate_request(api=UserHomeFeedAPI, user=group_user_public_workgroupuser.group_user.user)
